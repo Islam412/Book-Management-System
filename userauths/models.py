170c1a4921a292e29a430c2574fd6ba710cbff5d
Islam412 <islam.124.hamdy@gmail.com>
2024-09-10 16:04:43 +0300

    add create profile defult

--- userauths/models.py
+++ userauths/models.py
@@ -32,3 +32,15 @@ class Profile(models.Model):
         return self.user.username
     
 
+# create user profile automatic
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile ,sender=User)
+post_save.connect(save_user_profile ,sender=User)
