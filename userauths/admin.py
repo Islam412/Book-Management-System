37d7394b0ca9932114e92501083b077e045f52b4
Islam412 <islam.124.hamdy@gmail.com>
2024-09-10 15:51:55 +0300

    edite off admin off user

--- userauths/admin.py
+++ userauths/admin.py
@@ -4,6 +4,9 @@ from userauths.models import User
 # Register your models here.
 class UserCustomAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'username' , 'email']
+    search_fields = ['first_name' , 'last_name' , 'username']
+    list_filter = ['username']
+
     
 
 admin.site.register(User, UserCustomAdmin)
