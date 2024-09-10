6be8446b560ca648fd34f2dfb055bbc62f68e8a6
Islam412 <islam.124.hamdy@gmail.com>
2024-09-10 15:40:53 +0300

    add new migrate and edite admin off user p1

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from userauths.models import User
 
 # Register your models here.
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'username' , 'email']
+    
+
+admin.site.register(User, UserCustomAdmin)
