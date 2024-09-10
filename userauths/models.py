0e9ecc49b2d859b82f76e20f582954ff79423a10
Islam412 <islam.124.hamdy@gmail.com>
2024-09-10 16:14:57 +0300

    create profile defult

--- userauths/models.py
+++ userauths/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -31,14 +32,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
-
+@receiver(post_save, sender=User)
 # create user profile automatic
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         
 
-
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
