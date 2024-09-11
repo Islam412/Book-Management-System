0d706691e24ddc0549a0c8ab1c40b904d674fe0b
Islam412 <islam.124.hamdy@gmail.com>
2024-09-11 14:11:43 +0300

    add book in admin and create book custom admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookCustomAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'active', 'status']
+    list_filter = ['active','status']
+    search_fields = ['title','author','descripition']
+    
+
+admin.site.register(Book, BookCustomAdmin)
