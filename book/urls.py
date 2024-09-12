7a601c8c6ddeb63391316f634a82f737daae9b74
Islam412 <islam.124.hamdy@gmail.com>
2024-09-12 13:01:08 +0300

    create add book

--- book/urls.py
+++ book/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import home, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import home
 
 app_name = 'book'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Book
 from .forms import BookForm
@@ -11,29 +8,15 @@ from .forms import BookForm
 def home(request):
     return render(request, 'book/home.html')
 
-class BookListView(ListView):
-    model = Book
-    template_name = 'book/book_list.html'
-    context_object_name = 'books'
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book/book_detail.html'
-    context_object_name = 'book'
-
-class BookCreateView(CreateView):
-    model = Book
-    form_class = BookForm
-    template_name = 'book/create_book.html'
-    success_url = reverse_lazy('book:book-list')
-
-class BookUpdateView(UpdateView):
-    model = Book
-    form_class = BookForm
-    template_name = 'book/update_book.html'
-    success_url = reverse_lazy('book:book-list')
-
-class BookDeleteView(DeleteView):
-    model = Book
-    template_name = 'book/delete_book.html'
-    success_url = reverse_lazy('book:book-list')
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book added successfully!')
+            return redirect('book:book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book/add_book.html', {'form': form})
