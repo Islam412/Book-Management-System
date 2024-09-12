691fbab7315c49003c5eb46430dadcb99ad37052
Islam412 <islam.124.hamdy@gmail.com>
2024-09-12 13:03:09 +0300

    create add book urls

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home , add_book , 
 
 app_name = 'book'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('add/', add_book, name='add_book'),
+
 ]
