35512ec45f946170846846b47c1435d70fe6bdc8
Islam412 <islam.124.hamdy@gmail.com>
2024-09-12 12:25:09 +0300

    create book details view

--- book/views.py
+++ book/views.py
@@ -17,3 +17,8 @@ class BookListView(ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'books'
+    
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book/book_detail.html'
+    context_object_name = 'book'
