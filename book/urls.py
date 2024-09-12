0e4a44566dbce4f014389a2e23ed1679680d6dd0
Islam412 <islam.124.hamdy@gmail.com>
2024-09-12 12:35:26 +0300

    create urls for views

--- book/urls.py
+++ book/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-
-from .views import home
-
+from .views import home, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 app_name = 'book'
 
-
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- book/views.py
+++ book/views.py
@@ -7,34 +7,32 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Book
 from .forms import BookForm
 
-
 @login_required
 def home(request):
     return render(request, 'book/home.html')
-
 
 class BookListView(ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'books'
-    
+
 class BookDetailView(DetailView):
     model = Book
     template_name = 'book/book_detail.html'
     context_object_name = 'book'
-    
+
 class BookCreateView(CreateView):
     model = Book
     form_class = BookForm
     template_name = 'book/create_book.html'
     success_url = reverse_lazy('book:book-list')
-    
+
 class BookUpdateView(UpdateView):
     model = Book
     form_class = BookForm
     template_name = 'book/update_book.html'
     success_url = reverse_lazy('book:book-list')
-    
+
 class BookDeleteView(DeleteView):
     model = Book
     template_name = 'book/delete_book.html'
