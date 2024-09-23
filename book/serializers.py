c600f8af06705bee29770fe5d2c46f50454fc5bc
Islam412 <islam.124.hamdy@gmail.com>
2024-09-24 01:38:11 +0300

    create BookSerializer

--- book/serializers.py
+++ book/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book 
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
