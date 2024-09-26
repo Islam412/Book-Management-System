9d8e2d607d7d7cfc539884f3fc509bbb9d1e08b7
Islam412 <islam.124.hamdy@gmail.com>
2024-09-26 23:38:57 +0300

    creat functions with patch and fix erorr

--- book/views.py
+++ book/views.py
@@ -161,14 +161,21 @@ def book_detail(request, pk):
     """
     try:
         book = Book.objects.get(pk=pk)
-    except book.DoesNotExist:
+    except Book.DoesNotExist: # ModelName.DoesNotExist not object_name.DoesNotExist
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = BookSerializer(book)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    elif request.method == 'PUT': # scrol all data
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+     elif request.method == 'PATCH': # scrol chosiess data
         serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -178,3 +185,7 @@ def book_detail(request, pk):
     elif request.method == 'DELETE':
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# API with class based views
+
