aa91c67aadd23ef095986cf717f85220d9d81ee7
Islam412 <islam.124.hamdy@gmail.com>
2024-09-27 20:05:22 +0300

    create book details api and create put off pook details

--- book/views.py
+++ book/views.py
@@ -226,4 +226,12 @@ class BookDetail(APIView):
         book = self.get_object(pk)
         serializer = BookSerializer(book)
         return Response(serializer.data)
-    
+
+    def put(self, request, pk):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
