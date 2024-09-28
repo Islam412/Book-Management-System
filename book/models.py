3a4a59a682a5b56cf2d2217aeb31e0e796346022
Islam412 <islam.124.hamdy@gmail.com>
2024-09-28 15:42:21 +0300

    change defult off book and edite style off photo off books

--- book/models.py
+++ book/models.py
@@ -11,7 +11,7 @@ status_books = [
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200, blank=True)
-    photo_book = models.ImageField(upload_to='photos_books', null='True', blank=True, default='book.png')
+    photo_book = models.ImageField(upload_to='photos_books', null='True', blank=True, default='book.jpg')
     photo_author = models.ImageField(upload_to='photos_authors', null=True, blank=True)
     pages = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2,null=True, blank=True)
