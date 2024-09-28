05e26258d0a45979e75c8bcf2c389450a43a914a
Islam412 <islam.124.hamdy@gmail.com>
2024-09-28 18:46:56 +0300

    create profile serialzer api

--- userauths/serializers.py
+++ userauths/serializers.py
@@ -5,3 +5,8 @@ from .models import User, Profile
 class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = '__all__'
+    
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    model = Profile
+    fields = '__all__'
